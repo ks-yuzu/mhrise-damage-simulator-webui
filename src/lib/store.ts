1a656cdb5214cade8c233dbcee67bf59fddb82be
yuki.osako <yuki.osako@gree.net>
2023-02-19 15:30:05 +0900

    Add stores for simulation params and weapon type

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,8 +1,20 @@
 import {writable}                                from 'svelte/store'
+import {SHARPNESS}                               from 'mhrise-damage-simulator/sharpness'
 import {EnhancementMaster, EnhancementSelection} from '$lib/mhrise-enhancement'
 import {initialWeaponType}                       from '$lib/config'
 
 export const isSeriesVisible = writable({} as {[key: string]: boolean})
+
+export const simulationParams = writable({
+  weapon:                {attack: 330, element: 42, affinity: 0},
+  sharpness:             SHARPNESS.none,
+  motion:                {motionValue: 10, elementModifier: 1},
+  enhancements:          [],
+  monsterPartMultiplier: {physical: 55, elemental: 25},
+})
+
+export const weaponType = writable(initialWeaponType)
+
 const enhancementMaster = new EnhancementMaster(initialWeaponType)
 export const enhancementSelection = writable(new EnhancementSelection(enhancementMaster))
 
